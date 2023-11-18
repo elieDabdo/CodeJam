1f365539b4456b3593fa709e0af2b2ed597ab1f5
Anthony <anthony.tan2@mail.mcgill.ca>
2023-11-17 23:13:58 -0500

    add some home page stuff

---
+++ my_trainer/src/components/Home.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import NavBar from './NavBar/NavBar';
+
+const Home = () => {
+  return (
+    <>
+    <NavBar/>
+        <div>
+        <h1>Welcome to My Trainer</h1>
+        <p>This is the home page of our application.</p>
+        </div>
+    </>
+  );
+};
+
+export default Home;
