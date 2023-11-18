67befc8276c0afacdbeab15cd1273dbeb51714a8
Anthony <anthony.tan2@mail.mcgill.ca>
2023-11-17 23:38:34 -0500

    fix grid a bit

--- my_trainer/src/components/Grid/Grid.jsx
+++ my_trainer/src/components/Grid/Grid.jsx
@@ -6,8 +6,8 @@ const Grid = () => {
     return (
         <div class="grid-container">
             <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/05/11/84/12/1000_F_511841276_7MyhimdVvJUi5sftZiCRtaIUMG2siF6t.jpg" title="Preset 1" />
-            <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/05/11/84/12/1000_F_511841276_7MyhimdVvJUi5sftZiCRtaIUMG2siF6t.jpg" title="Preset 2" />
-            <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/05/11/84/12/1000_F_511841276_7MyhimdVvJUi5sftZiCRtaIUMG2siF6t.jpg" title="Preset 3" />
+            <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/01/90/99/76/1000_F_190997632_qvZw0IkKXRyQxzatlpOBsSqW0WVg8SDp.jpg" title="Preset 2" />
+            <Preset pictureUrl="https://images.pexels.com/photos/3822906/pexels-photo-3822906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" title="Preset 3" />
             <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/05/11/84/12/1000_F_511841276_7MyhimdVvJUi5sftZiCRtaIUMG2siF6t.jpg" title="Preset 4" />
             <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/05/11/84/12/1000_F_511841276_7MyhimdVvJUi5sftZiCRtaIUMG2siF6t.jpg" title="Preset 5" />
             <Preset pictureUrl="https://as1.ftcdn.net/v2/jpg/05/11/84/12/1000_F_511841276_7MyhimdVvJUi5sftZiCRtaIUMG2siF6t.jpg" title="Preset 6" />
