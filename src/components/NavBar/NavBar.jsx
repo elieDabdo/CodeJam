47e75d30a922cf0ff9a3cb8875c89c2d97e751a9
MrMondrian <57821698+MrMondrian@users.noreply.github.com>
2023-11-18 17:04:37 -0500

    make title a link back to home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react';
 import './NavBar.css';
+import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <nav className="navbar">
             <div className="nav-main">
                 <div className="name">
-                    <h2>My Trainer</h2>
+                    <Link to="/Codejam"><h2>My Trainer</h2></Link>
                 </div>
             </div>
         </nav>
