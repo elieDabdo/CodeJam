faa186876b6514c529ebe2c6121ad175ed83e388
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-11-18 07:34:31 -0500

    interim

--- my_trainer/src/components/Playback.jsx
+++ my_trainer/src/components/Playback.jsx
@@ -29,75 +29,42 @@ webcamPose.setOptions(poseOptions);
 // DEFINE POSE DETECTION CALLBACK FUNCTION
 trainingPose.onResults(onTrainingPose);
 webcamPose.onResults(onWebcamPose);
-
-let init = false;
   
 function Playback({ video_url, user_params }) {
-    const webcamRef = useRef(null);
-
     const minimizedProps = {className:"min-player", height:'30%', width:'500vh'};
     const maximizedProps = {className:"max-player", height:'100vh', width:'100%'};
 
     const trainingVideoProps = user_params.training_video_maximized ? maximizedProps : minimizedProps;
     const webcamPlayerProps = user_params.training_video_maximized ? minimizedProps : maximizedProps;
 
-
-    useEffect(() => {
-        if (init) return () => {};
-        init = true;
-        console.log(webcamRef.current);
-        
-        const camera = new Camera(webcamRef.current, {
-            onFrame: async () => {
-                // console.log("webcam frame");
-                // if (webcamRef.current.videoWidth) webcamPose.send({ image: webcamRef.current })
-            },
-            width: 1280,
-            height: 720,
-        });
-    
-        camera.start();
-        
-        // Cleanup function (optional) to be executed on component unmount
-        return () => {
-            camera.stop();
-        };
-      });
-
     // DEFINE FRAME CALLBACKS
     const handleTrainingVideoFrame = async (video) => {
+        await trainingPose.send({image: video})
+        //run media pipe pose model on frame and get landmark information
+    }
+    
+    // DEFINE FRAME CALLBACKS
+    const handleWebcamVideoFrame = async (video) => {
         console.log(video);
-        console.log(video.videoWidth)
-        trainingPose.send({image: video})
+        await trainingPose.send({image: video})
         //run media pipe pose model on frame and get landmark information
     }
     
     return (
       <div>
         {/* TRAINING VIDEO */}
-        <VideoPlayer
+        {/* <VideoPlayer
             video_url={video_url}
             props={trainingVideoProps}
             onFrame={handleTrainingVideoFrame}
-        />
+        /> */}
       
         {/* WEBCAM INPUT */}
-        <div className={webcamPlayerProps.className}>
-            <video
-                className='input_video'
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    transform: 'scaleX(-1)',
-                }}
-                ref={webcamRef}
-                controls={false}
-                autoPlay={true}
-            ></video>
-        </div>
+        <VideoPlayer
+            webcam={true}
+            props={webcamPlayerProps}
+            onFrame={handleWebcamVideoFrame}
+        />
       </div>
     );
   }
--- my_trainer/src/components/VideoPlayer.jsx
+++ my_trainer/src/components/VideoPlayer.jsx
@@ -1,43 +1,78 @@
 import React, { useRef, useEffect } from 'react';
+import { Camera } from '@mediapipe/camera_utils/camera_utils.js';
 
-function VideoPlayer({ video_url, props, onFrame }) {
+function VideoPlayer({ webcam, video_url, props, onFrame }) {
   const videoRef = useRef(null);
 
-  const handleNewFrame = () => {
-    onFrame(videoRef.current)
-  };
-
   useEffect(() => {
-    const intervalId = setInterval(handleNewFrame, 1000);
-
-    // Cleanup function to clear the interval when the component unmounts
-    return () => {
-      clearInterval(intervalId);
+    const handleNewFrame = () => {
+      onFrame(videoRef.current)
     };
-  });
+    if (webcam) {  
+      const startWebcam = async () => {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          videoRef.current.srcObject = stream;
+        } catch (error) {
+          console.error('Error accessing webcam:', error);
+        }
+      };
+  
+      startWebcam();
+      videoRef.current.addEventListener('timeupdate', handleNewFrame);
+  
+      return () => {
+        // Cleanup: Stop the webcam stream when the component unmounts
+        if (webcam && videoRef.current) {
+          const stream = videoRef.current.srcObject;
+          if (stream) {
+            const tracks = stream.getTracks();
+            tracks.forEach((track) => track.stop());
+          }
+        }
+      };
+    } else {
+      const intervalId = setInterval(handleNewFrame, 1000);
+      return () => { clearInterval(intervalId); }
+    }
+  }, [webcam, onFrame]);
 
   return (
     <div>
       {/* Video Player */}
       <div className={props.className}>
-        <video
-          className='input_video'
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-          ref={videoRef}
-          controls={true}
-          width={props.width}
-          height={props.height}
-          autoPlay={true}
-          src={video_url}
-        >
-          {/* <source src={video_url} type="video/mp4" /> */}
-        </video>
+        {webcam ? (
+          <video
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              transform: 'scaleX(-1)',
+            }}
+            ref={videoRef}
+            controls={false}
+            autoPlay={true}
+          ></video>
+        ) : (
+          <video
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            ref={videoRef}
+            controls={true}
+            width={props.width}
+            height={props.height}
+            autoPlay={true}
+          >
+            <source src={video_url} type="video/mp4" />
+          </video>
+        )}
       </div>
     </div>
   );
