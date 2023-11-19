4f7a3314240bb2d71f67e44381ba1f14d243afbf
MrMondrian <57821698+MrMondrian@users.noreply.github.com>
2023-11-18 21:14:02 -0500

    add buttons

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,20 +2,26 @@
 import React, {useLocation} from 'react-router-dom';
 import Playback from '../Playback';
 import Navbar from '../Navbar';
-
+import Footer from '../Footer';
+import ScrollToTop from '../ScrollToTop';
 
 const Game = () => {
     const location = useLocation();
     const video = location.state.video;
     return (
-        <Playback video_url={video} user_params={
-            {
-              training_video_maximized:true,
-              draw_webcam_skeleton_on_webcam:true,
-              draw_webcam_skeleton_on_training:true,
-              draw_training_skeleton_on_training:true,
-              draw_training_skeleton_on_webcam:true
-            }} />
+        <>
+          <Navbar />
+          <ScrollToTop />
+          <Playback video_url={video} user_params={
+              {
+                training_video_maximized:true,
+                draw_webcam_skeleton_on_webcam:true,
+                draw_webcam_skeleton_on_training:true,
+                draw_training_skeleton_on_training:true,
+                draw_training_skeleton_on_webcam:true
+              }} />
+          <Footer />
+        </>
     );
 };
 
